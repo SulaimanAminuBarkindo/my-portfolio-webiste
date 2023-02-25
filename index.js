7ea57404d5dd2c658ce4b2492260ffce048983e9
Sulaiman Aminu Barkindo <sulaimanaminu02@gmail.com>
2023-02-25 11:06:32 +0100

    Fix nav bar for mobile view

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const body = document.querySelector('body')
 
 menuIcon.addEventListener('click', toggleMenu)
 
+window.onscroll = function() {
+    scrollFunction()
+}
+
 function toggleMenu(){
     menuIcon.classList.toggle('open-menu-icon')
     menuContainer.classList.toggle('hide-menu-container')
@@ -16,5 +20,15 @@ function showExpText(className) {
     console.log(sectToShow)
     openedSect.classList.toggle('show-experience-text')
     sectToShow.classList.toggle('show-experience-text')
+}
+
+function scrollFunction() {
+    if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        const text = document.querySelector('.nav-container').style.backgroundColor = '#0a192ff8'
+        document.querySelector('.nav-container').style.boxShadow = '0px 0px 10px rgba(2, 12, 27)'
+    } else {
+        document.querySelector('.nav-container').style.backgroundColor = '#0a192f'
+        document.querySelector('.nav-container').style.boxShadow = '0px 0px 0px'
+    }
 }
 
