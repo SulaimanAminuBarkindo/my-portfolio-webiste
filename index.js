e57a695005577671684516e9d1c76a8ebd0be843
Sulaiman Aminu Barkindo <sulaimanaminu02@gmail.com>
2023-02-27 21:23:46 +0100

    Fix work section

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const menuIcon = document.querySelector('.menu-icon')
 const menuContainer = document.querySelector('.menu-container')
 const body = document.querySelector('body')
+const frame = document.querySelector('.image-frame')
 
 menuIcon.addEventListener('click', toggleMenu)
 
@@ -17,18 +18,30 @@ function toggleMenu(){
 function showExpText(className) {
     const sectToShow = document.querySelector(`.${className}`)
     const openedSect = document.querySelector('.show-experience-text')
-    console.log(sectToShow)
+ 
     openedSect.classList.toggle('show-experience-text')
     sectToShow.classList.toggle('show-experience-text')
 }
 
 function scrollFunction() {
     if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        const text = document.querySelector('.nav-container').style.backgroundColor = '#0a192ff8'
+        document.querySelector('.nav-container').style.backgroundColor = '#0a192ff8'
         document.querySelector('.nav-container').style.boxShadow = '0px 0px 10px rgba(2, 12, 27)'
     } else {
         document.querySelector('.nav-container').style.backgroundColor = '#0a192f'
         document.querySelector('.nav-container').style.boxShadow = '0px 0px 0px'
     }
+}
+
+function moveFrame() {
+    frame.classList.toggle('image-frame-over')
+}
+
+function makeActive(className) {
+    const divToActivate = document.querySelector(`.${className}`)
+    const divToDeactivate = document.querySelector('.active-work-place')
+
+    divToDeactivate.classList.toggle('active-work-place')
+    divToActivate.classList.toggle('active-work-place')
 }
 
